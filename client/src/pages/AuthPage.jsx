2a4cc91eedef087fcd8a794b96010a90cc8fd468
deividas <tamosaitis.deividas@gmail.com>
2025-08-25 22:58:37 +0300

    ner registracijos

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -66,6 +66,7 @@ export default function AuthPage() {
             </button>
             <button
               type="button"
+              disabled
               role="tab"
               aria-selected={mode === "signup"}
               className={`flex-1 rounded-xl px-4 py-2 font-medium transition ${
